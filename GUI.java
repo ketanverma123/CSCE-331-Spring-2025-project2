cef189c0cce76b22abe795162790278721d47b8f
landondelgado <landon.delgado@gmail.com>
2025-02-21 09:15:11 -0600

    Initial GUI Commit

---
+++ GUI.java
@@ -0,0 +1,101 @@
+import java.sql.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/*
+  TODO:
+  1) Change credentials for your own team's database
+  2) Change SQL command to a relevant query that retrieves a small amount of data
+  3) Create a JTextArea object using the queried data
+  4) Add the new object to the JPanel p
+*/
+
+public class GUI extends JFrame implements ActionListener {
+    static JFrame f;
+
+    public static void main(String[] args)
+    {
+      //Building the connection
+      Connection conn = null;
+      //TODO STEP 1 (see line 7)
+      String database_name = "team_74_db";
+      String database_user = "team_74";
+      String database_password = "alka";
+      String database_url = String.format("jdbc:postgresql://csce-315-db.engr.tamu.edu/%s", database_name);
+      try {
+        conn = DriverManager.getConnection(database_url, database_user, database_password);
+      } catch (Exception e) {
+        e.printStackTrace();
+        System.err.println(e.getClass().getName()+": "+e.getMessage());
+        System.exit(0);
+      }
+      JOptionPane.showMessageDialog(null,"Opened database successfully");
+
+      String name = "";
+      try{
+        //create a statement object
+        Statement stmt = conn.createStatement();
+        //create a SQL statement
+        //TODO Step 2 (see line 8)
+        String sqlStatement = "SELECT ItemName, Category, COUNT(*) AS NumberOfSales FROM Sales GROUP BY ItemName, Category ORDER BY NumberOfSales DESC;";
+        //send statement to DBMS
+        ResultSet result = stmt.executeQuery(sqlStatement);
+        while (result.next()) {
+          // TODO you probably need to change the column name tat you are retrieving
+          //      this command gets the data from the "name" attribute
+          name += result.getString("ItemName") + "   " + result.getString("NumberOfSales")+"\n";
+        }
+      } catch (Exception e){
+        JOptionPane.showMessageDialog(null,"Error accessing Database.");
+      }
+      // create a new frame
+      f = new JFrame("DB GUI");
+
+      // create a object
+      GUI s = new GUI();
+
+      // create a panel
+      JPanel p = new JPanel();
+
+      JButton b = new JButton("Close");
+
+      // add actionlistener to button
+      b.addActionListener(s);
+
+      //TODO Step 3 (see line 9)
+      JTextArea textArea = new JTextArea(15,30);
+      textArea.setText(name.toString());
+
+      //TODO Step 4 (see line 10)
+      p.add(textArea);
+
+      // add button to panel
+      p.add(b);
+
+      // add panel to frame
+      f.add(p);
+
+      // set the size of frame
+      //f.setSize(400, 400);
+      f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+      f.setVisible(true);
+
+      //closing the connection
+      try {
+        conn.close();
+        JOptionPane.showMessageDialog(null,"Connection Closed.");
+      } catch(Exception e) {
+        JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
+      }
+    }
+
+    // if button is pressed
+    public void actionPerformed(ActionEvent e)
+    {
+        String s = e.getActionCommand();
+        if (s.equals("Close")) {
+            f.dispose();
+        }
+    }
+}
